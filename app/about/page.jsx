e4d7938ec15c3164a52e184bcefb483ab7ce9af0
Marlon Sanchez <Trugrey@gmail.com>
2024-05-30 11:14:07 -0400

    .

---
+++ app/about/page.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return(
+        <>
+            <h1>Hey baby</h1>
+        </>
+    )
+}
---
+++ app/components/TopBar.jsx
@@ -0,0 +1,5 @@
+export default function TopBar() {
+    return(
+        <h1>The Top Bar</h1>
+    )
+}
